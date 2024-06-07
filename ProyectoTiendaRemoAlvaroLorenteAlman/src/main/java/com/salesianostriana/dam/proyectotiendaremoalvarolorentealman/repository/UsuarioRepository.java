27bc84bb1ffa97f833dcbdd5792f7b6c8ef60f68
Álvaro Lorente Almán <lorente.alalv24@triana.salesianos.edu>
2024-06-07 10:54:17 +0200

    clase usuarioController, usuarioService y vistas

--- ProyectoTiendaRemoAlvaroLorenteAlman/src/main/java/com/salesianostriana/dam/proyectotiendaremoalvarolorentealman/repository/UsuarioRepository.java
+++ ProyectoTiendaRemoAlvaroLorenteAlman/src/main/java/com/salesianostriana/dam/proyectotiendaremoalvarolorentealman/repository/UsuarioRepository.java
@@ -9,11 +9,17 @@ import org.springframework.data.jpa.repository.Query;
 import com.salesianostriana.dam.proyectotiendaremoalvarolorentealman.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
 	Optional<Usuario> findFirstByUsername(String username);
 	Usuario findByUsername(String username);
 	
+	@Query("select u.id from Usuario u")
+	public List<Long> obtenerIds();
 	
+
 	@Query("select u from Usuario u where TYPE(u) = Usuario")
 	public List<Usuario> usuarioNoCliente();
+	
+	
 
 }
