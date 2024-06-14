df6aaab0aa0b8969f89759fd59f47420cb610e78
Álvaro Lorente Almán <lorente.alalv24@triana.salesianos.edu>
2024-06-14 12:34:39 +0200

    seguridad

--- ProyectoTiendaRemoAlvaroLorenteAlman/src/main/java/com/salesianostriana/dam/proyectotiendaremoalvarolorentealman/security/PasswordEncoderConfig.java
+++ ProyectoTiendaRemoAlvaroLorenteAlman/src/main/java/com/salesianostriana/dam/proyectotiendaremoalvarolorentealman/security/PasswordEncoderConfig.java
@@ -4,10 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-<<<<<<< HEAD
 
-=======
->>>>>>> 68fffa0ecb919a4dff89356a22aac52d875ba7e8
 @Configuration
 public class PasswordEncoderConfig {
 	@Bean
