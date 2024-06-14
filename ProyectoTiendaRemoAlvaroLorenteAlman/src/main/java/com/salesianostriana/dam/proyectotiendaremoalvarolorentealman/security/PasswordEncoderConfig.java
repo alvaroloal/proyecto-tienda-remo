9a59eff3ebea1c43282520cf4dab62b1ae0ae57e
Álvaro Lorente Almán <lorente.alalv24@triana.salesianos.edu>
2024-06-14 09:35:35 +0200

    modificando clase usuario

--- ProyectoTiendaRemoAlvaroLorenteAlman/src/main/java/com/salesianostriana/dam/proyectotiendaremoalvarolorentealman/security/PasswordEncoderConfig.java
+++ ProyectoTiendaRemoAlvaroLorenteAlman/src/main/java/com/salesianostriana/dam/proyectotiendaremoalvarolorentealman/security/PasswordEncoderConfig.java
@@ -1,9 +1,10 @@
 package com.salesianostriana.dam.proyectotiendaremoalvarolorentealman.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+@Configuration
 public class PasswordEncoderConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
